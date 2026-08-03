a96e5c696ee6f65eb1cbfb5e4723e61c21a01a3d
ArifD <arifdanial.azrul@gmail.com>
2026-08-03 23:11:49 +0800

    Update 4.1
    Notes:
    Changes:
    - Progress on tutorial mechanics

--- strandbeest-game/crab.gd
+++ strandbeest-game/crab.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 
 
 @onready var animation = $AnimatedSprite2D
-@onready var play = get_node("/root/UI/Tutorial")
+@onready var tutorial = get_node("/root/Node2D/UI/Tutorial")
 
 var loop_amount: float = 3.0
 
-if 
+func _process(delta: float) -> void:
+	# Reminder: To get a variable from a different node, you first must use get_node 
+	# and then add the node's name, (node name.variable name)
+	if tutorial.tutorial_playing == true:
+		play_animation()
 
-func _ready():
-	play_animation()
 
 func play_animation():
 	animation.animation_finished.connect(_on_animation_finished)
--- strandbeest-game/tutorial.gd
+++ strandbeest-game/tutorial.gd
@@ -1,1 +1,44 @@
 extends Control
+
+# Notes:
+# First, we make a condition where only if the start tutorial button is pressed,
+# only then the tutorial sequence starts playing.
+# We add a back and next button instead of just "space to continue" because
+# some people might accidentally press continue, coudln't step back,
+# and has to repeat through the whole sequence of the tutorial again.
+
+# Talking about sequence of the tutorial, we can use a similar mechanic in wave_bottom.gd - states
+
+# Code to start tutorial
+@onready var dialogue = $Dialogue
+@onready var back_button = $"Back Button"
+@onready var next_button = $"Next Button"
+@onready var start_tutorial = $"Start Tutorial"
+
+
+var tutorial_playing: bool = false
+var _state: String = "Tutorial finished"
+
+
+func _ready() -> void:
+	dialogue.visibile = false
+	back_button.visible = false
+	next_button.visible = false
+	
+func _process(delta: float) -> void:
+	if start_tutorial.pressed:
+		tutorial_playing = true
+	if tutorial_playing == true:
+		match _state:
+			"Phase 1":
+				dialogue.visible = true
+				next_button.visible = true
+				dialogue.text = "Hello player, welcome to Strandbeest Game! I am Mr Crabs but you can call me ..."
+				if next_button.pressed:
+					_state = "Phase 2"
+			"Phase 2":
+				dialogue.visible = true
+				next_button.visible = true
+				back_button.visible = true
+				dialogue.text = "This game "
+				
