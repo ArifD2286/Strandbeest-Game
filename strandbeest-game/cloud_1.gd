f7b9423ed97ac0580d3deb46123327b2a289f324
ArifD <arifdanial.azrul@gmail.com>
2026-08-14 16:57:10 +0800

    Update 3.6
    Changes:
    - Continued working on the player's cloud animation mechanic

--- strandbeest-game/cloud_1.gd
+++ strandbeest-game/cloud_1.gd
@@ -1,30 +1,38 @@
 extends Sprite2D
 
 # Notes:
-# Similar mechanics to the waves, each layer would have its own movement script so that it has the "fluffy" effect,
-# this adds realism instead of just 1 static cloud design.
-# But it would also have similar mechanicsto the wave_back.gd, the [_state].
+# We can use states again to have variation of animation for different scenarios.
 
+# pull_fraction in player.gd is a var with a smooth change from 0 to 1,
+# we can use this for the shift of position of the cloud.
+# Say that 0 is when the cloud sits at its resting pos and 1 is where the cloud sits at its target spot.
+
+# For the movement of the clouds, we can use a [.lerp()] (lerp is short for linear interpolation).
+# It is an operation for Vector2 for blending in between two points using a "0 to 1".
+
+# Vector2.ZERO is the target point for cloud1/2/3.gd to go to from their own initial position
+# as [position] are local coordinates for the Player (CharacterBody2D) node.
+
+@export var charge_target: Vector2 = Vector2.ZERO
 @onready var player = get_parent()
 
 @export var radius: float = 10.0
-@export var charging_radius: float = 2.0
 @export var burst_radius: float = 20.0
 
-# The variables below doesn't make the duration for the animation state, it is only for the speed of the circle movement / how long
-# it takes for 1 whole loop.
-# For the duration of burst state (since its the only independent animation, whereas idle and charging relies on the player),
-# it is declared in player.gd
-@export var cycle_seconds: float = 5.0
-@export var charging_seconds: float = 1.0
-@export var burst_seconds: float = 3.0
+# The variables below doesn't make the duration for the animation state,
+# it is only for the speed of the circle movement / how long it takes for 1 whole loop.
 
+# For the duration of burst state (since its the only independent animation,
+# whereas idle and charging relies on the player),
+# it is declared in player.gd
+
+@export var cycle_seconds: float = 5.0
+@export var burst_seconds: float = 3.0
 @export var phase_offset: float = 0.0
 
 var _rest_position: Vector2
 var _omega: float
 var _t: float
-
 
 func _ready() -> void:
 	_t = phase_offset
@@ -32,25 +40,18 @@ func _ready() -> void:
 	# it would store both x and y values in vector form.
 	_rest_position = position
 
-	
+
 func _process(delta: float) -> void:
 	match player._state:
 		"Idle":
 			_t += delta
 			_omega = TAU / cycle_seconds
-			# Since [_rest_position] is a vector, we want to only take the x or y value in the equation,
-			# so we can add either [.x] or [.y] behind the [_rest_position].
-			# This works similarly to [position.x] or [position.y] where the .x or .y would state which part of the coordinate we want.
 			position.x = _rest_position.x + radius * sin(_omega * _t)
 			position.y = _rest_position.y + radius * cos(_omega * _t)
-
 		"Charging":
 			_t += delta
-			_omega = TAU / charging_seconds
-			position.x = _rest_position.x + charging_radius * sin(_omega * _t)
-			position.y = _rest_position.y + charging_radius * cos(_omega * _t)
+			position = _rest_position.lerp(charge_target, player.pull_fraction)
 		"Burst":
 			_t += delta
-			_omega = TAU / burst_seconds
 			position.x = _rest_position.x + burst_radius * sin(_omega * _t)
 			position.y = _rest_position.y + burst_radius * cos(_omega * _t)
--- strandbeest-game/player.gd
+++ strandbeest-game/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-
-@export var charge_duration: float = 2.0
+@onready var tutorial = get_node("/root/Node2D/UI/Tutorial")
+@export var charge_duration: float = 0.5
 
 var pull_fraction: float = 0.0
 var _charge_t: float = 0.0
 var _state: String = "Idle"
 var _t: float = 0.0
-var reset_time: float = 2.0
+var reset_time: float = 0.0
 
 
 func _process(delta: float) -> void:
@@ -15,14 +15,20 @@ func _process(delta: float) -> void:
 	velocity.x = 0
 
 # If the player presses onto their space button, the state would go into the charging state
-	if _state == "Idle" and Input.is_action_just_pressed("shooting"):
+	if _state == "Idle" and Input.is_action_just_pressed("shooting") and not tutorial.tutorial_playing == true:
 		_state = "Charging"
 		_charge_t = 0.0
 
-# If they release their space button, it goes into the shooting state.
+# If they release their space button, it goes into the bursting/shooting wind state.
 # This state will be referred by cloud_1/2/3.gd for their animation/behaviour
-	elif _state == "Charging" and Input.is_action_just_released("shooting"):
+	elif _state == "Charging" and Input.is_action_just_released("shooting") and not tutorial.tutorial_playing == true:
 		_state = "Burst"
+		_t = 0.0
+
+# We also want to put a condition where the player presses DURING the "Burst" state
+	elif _state == "Burst" and Input.is_action_just_pressed("shooting") and not tutorial.tutorial_playing == true:
+		_state = "Charging"
+		_charge_t = 0.0
 
 	if _state == "Charging":
 		_charge_t += delta
