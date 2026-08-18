d58fbcde0b54131f251a9fa75609f29056ca2354
ArifD <arifdanial.azrul@gmail.com>
2026-08-18 09:42:07 +0800

    Update 5.4 (minor changes)
    Changes:
    - Added code so that the text on button (cost of upgrade) updates during start of game.

--- strandbeest-game/upgrade_2.gd
+++ strandbeest-game/upgrade_2.gd
@@ -3,5 +3,9 @@ extends Button
 # Notes:
 # - To update the cost for upgrades on the button's text
 
+func _ready() -> void:
+	GameData.get_speed_upgrade_cost()
+	GameData.get_momentum_upgrade_cost()
+
 func _process(delta: float) -> void:
 	text = str(GameData.momentum_cost) + " Pipes"
